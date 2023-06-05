47e6d5b6359af4d63b0ed1e32a7827ba5ecd34c9
Thai Bui <buithaitb911@gmail.com>
2023-06-05 22:14:50 +0700

    error handle

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,17 +1,26 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 exports.verifyToken = (req, res, next) => {
-    // Lay quyen truy cap tu req header
-    const Authorization = req.header('authorization')
-    if (!Authorization){
-        const err = new Error('Unauthorized')
-        err.statusCode = 400
-        return next(err)
-    }
-    // get token
-    const token = Authorization.replace("Bearer ", "")
+  const Authorization = req.header("authorization");
+  if (!Authorization) {
+    const err = new Error("Unauthorized");
+    err.statusCode = 400;
+    return next(err);
+  }
+  // get token
+  const token = Authorization.replace("Bearer ", "");
+  try {
     // Verify token
-    const {userId} = jwt.verify(token, process.env.APP_SECRET)
-    req.body['userId'] = userId
-    next()
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    console.log("User Id : ", userId);
+    req.body.userId = userId;
+    next();
+  } catch (error) {
+    logger.error(`Authorization fail", "ERROR": "${error.message}`);
+    if (error.message === "jwt expired") {
+      return res.status(400).json({ message: error.message });
+    } else {
+      return res.status(400).json({ message: "failure" });
+    }
+  }
 }
