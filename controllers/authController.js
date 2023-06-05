be9b55779c9d15591c0248c3dd93826dd123e9f0
Thai Bui <buithaitb911@gmail.com>
2023-06-05 22:25:39 +0700

    fix register, login

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,39 +4,51 @@ const bcrypt = require('bcryptjs')
 const cloudinary = require("../cloudinary/cloudinary");
 exports.register = async (req, res, next) => {
     try {
-        const result = await cloudinary.uploader.upload(req.file.path);
-        const user = await User.create({ ...req.body, avatar:result.secure_url, cloudinary_id:result.public_id})
-        const token = jwt.sign({userId: user._id}, process.env.APP_SECRET)
-        res.status(200).json({
-            status: 'sucsess',
-            data: {token, userName: user.name, avatar:result.secure_url, cloudinary_id:result.public_id}
-        })
+      const user = await User.create({ ...req.body, avatarurl: "" });
+      res.status(200).json({
+        status: "success",
+        data: { userName: user.name, avatarUrl: user.avatarurl }
+      });
+      console.log(`registered successfully",\"userId\": \"${user._id}`)
     } catch (error) {
-        next(error)
+      console.log(`register fail: ${error}`);
+      const err = { message: "Email exits", status: 400 };
+      next(err);
     }
 }
 exports.login = async (req, res, next) => {
     try {
-        const user = await User.findOne({email:req.body.email})
-        if(!user){
-            const err = new Error('Email is not correct')
-            err.statusCode = 400
-            return next(err)
+        const tokenTime = { expiresIn: "24h" };
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+          const err = { message: "Email or password is not correct", status: 400 };
+          next(err);
         }
-        if(bcrypt.compareSync(req.body.password, user.password)){ // (mật khẩu thật, mật khẩu đã hash)
-            const token = jwt.sign({userId: user._id}, process.env.APP_SECRET)
-            res.status(200).json({
-            status: 'sucsess',
-            data: {token, userName: user.name, id: user._id ,avatar: user.avatar , cloudinary_id: user.cloudinary_id}
-            })
-        }else{
-            const err = new Error('Password is not correct')
-            err.statusCode = 400
-            return next(err)
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          const token = jwt.sign(
+            { userId: user._id },
+            process.env.APP_SECRET,
+            tokenTime
+          );
+          res.status(200).json({
+            status: "success",
+            data: {
+              token,
+              userName: user.name,
+              id: user._id,
+              avatarUrl: user.avatarurl,
+              role: user.role
+            }
+          });
+          console.log(`Log-in successfully", \"userId\": \"${user._id}`);
+        } else {
+          const err = { message: "Email or password is not correct", status: 400 };
+          next(err);
         }
-    } catch (error) {
-        next(error)
-    }
+      } catch (error) {
+        console.log(`Log-in system's failed: ${error}`);
+        next(error);
+      }
 }
 exports.getCurrentUser = async (req, res, next) => {
     try {
