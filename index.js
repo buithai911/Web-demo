528a1ee946e9b1abdc8798b3b813d796a4bdb5ec
Thai Bui <buithaitb911@gmail.com>
2023-06-05 22:29:56 +0700

    remove sth

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-require('dotenv').config()
-const {errorHandler} = require('./middleware/errorHandler')
-const { connectDB } = require('./config/db')
-connectDB()
+require('dotenv').config();
+const { connectDB } = require('./config/db');
+connectDB();
 
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const cors = require('cors')
+const app = express();
+const cors = require('cors');
 
-const authRoute = require('./routers/authRoute')
-const productRoute = require('./routers/productRoute')
-const userRoute = require('./routers/userRoute')
+const authRoute = require('./routers/authRoute');
+const productRoute = require('./routers/productRoute');
+const userRoute = require('./routers/userRoute');
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/v1/auth', authRoute)
-app.use('/api/v1/products', productRoute)
-app.use('/api/v1/user', userRoute)
-app.get('/', (req, res) => res.send('halo'))
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/products', productRoute);
+app.use('/api/v1/user', userRoute);
+app.get('/', (req, res) => res.send('halo'));
 
 //err
 app.all('*', (req, res, next) => {
-  const err = new Error('Page not found')
-  err.statusCode = 404
-  next(err)
+  const err = new Error('Page not found');
+  err.statusCode = 404;
+  next(err);
 })
-app.use(errorHandler)
 
-const port = process.env.APP_PORT
+const port = process.env.APP_PORT;
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`))
